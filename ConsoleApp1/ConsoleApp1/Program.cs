9cb4ee2f425ac0f73d2dda0688e291d5e104fdc1
wewenami <yerezhepabdulla@gmail.com>
2019-11-07 19:49:13 +0100

    cool

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wewenami You are here");
+            Console.WriteLine("one more extra line");
+        }
+    }
+}
