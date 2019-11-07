cea13fc43f388cc48caf6c0f9728c49fee78f8a0
wewenami <yerezhepabdulla@gmail.com>
2019-11-07 20:27:06 +0100

    GitHub ex 3

---
+++ ConsoleApp1/exercises methods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Math;
+
+namespace exercises_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"-10{MathUtilities.IsPrime(-10)}");
+            Console.WriteLine($"1{MathUtilities.IsPrime(1)}");
+            Console.WriteLine($"23{MathUtilities.IsPrime(23)}");
+            Console.WriteLine($"0{MathUtilities.IsPrime(0)}");
+            Console.WriteLine($"8{MathUtilities.IsPrime(8)}");
+            Console.WriteLine($"76{MathUtilities.IsPrime(76)}");
+            Console.WriteLine($"9{MathUtilities.IsPrime(9)}");
+        }
+    }
+}
