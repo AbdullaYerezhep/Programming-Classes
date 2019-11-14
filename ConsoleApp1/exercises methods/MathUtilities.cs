908e0b6955cb8003e0cb57a97476ff504c6c1421
W59098 <W59098@RA117A-12>
2019-11-14 20:06:33 +0100

    .

--- ConsoleApp1/exercises methods/MathUtilities.cs
+++ ConsoleApp1/exercises methods/MathUtilities.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Math
+namespace Maths
 {
     public static class MathUtilities
     {
-        public static bool IsPrime(int number)
-
+            public static bool IsPrime(int number)
+            
             public static int Gcd(int a, int b)
             {
             while (a != b)
@@ -20,10 +20,37 @@ namespace Math
                 }
                  return a; 
             }
+            
             public static bool AreCoprime(int a, int b)
             {
                return Gcd(a,b) == 1;
                 
+            }
+
+            public static double[] SolveQuadratic(int  a, int  b, int c)
+            {
+                if (a == 0)
+                   throw new ArgumentException("a cannot be zero");
+
+                int delta = b * b - 4 * a * c;
+
+                if (delta > 0)
+                {
+                  // double x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+                  // double x2 = (-b + Math.Sqrt(delta)) / (2 * a);
+                  // double[] result = new double[2];
+                  // result[0] = x1;
+                  // result[1] = x2; 
+                   return new[2] { x1, x2 };
+
+                }
+                if (delta == 0)
+                {
+                double x1 = -b / (2 * a);
+                return new[] { x1 };
+                }
+                return new  double[0];
+            
             }
     }
 }
--- ConsoleApp1/exercises methods/Program.cs
+++ ConsoleApp1/exercises methods/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using Math;
+using Maths;
 
-namespace exercises_methods
+namespace BasicMethods
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(MathUtilities.AreCoprime(8,9));
-            Console.WriteLine(MathUtilities.AreCoprime(8,10));
-            
+            //Console.WriteLine(Maths.AreCoprime(8,9));
+            //Console.WriteLine(Maths.AreCoprime(8,10));
+            var solution1 = MathUtilities.SolveQuadratic(1, 5, 6);
+            foreach (var solution in solutions)
+            {
+                Console.WriteLine(solution);
+            }
         }
     }
 }
