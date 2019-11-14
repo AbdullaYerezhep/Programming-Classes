c80d39de5bf2f0eb4776fd86ff42cc04fa9f93f5
W59098 <W59098@RA117A-12>
2019-11-14 19:28:57 +0100

    Gcd method implemented

--- ConsoleApp1/exercises methods/MathUtilities.cs
+++ ConsoleApp1/exercises methods/MathUtilities.cs
@@ -7,15 +7,23 @@ namespace Math
     public static class MathUtilities
     {
         public static bool IsPrime(int number)
-        {
-            if (number < 2)
-                return false;
-            for (int i = 2; i < number; i++)
-                if (number % i == 0)
-                    return false;
 
-            return true;
+            public static int Gcd(int a, int b)
+            {
+            while (a != b)
+                {
+                if ( a > b)
+                    a -= b;
+                else
+                    b -= a;
             
-        }
+                }
+                 return a; 
+            }
+            public static bool AreCoprime(int a, int b)
+            {
+               return Gcd(a,b) == 1;
+                
+            }
     }
 }
--- ConsoleApp1/exercises methods/Program.cs
+++ ConsoleApp1/exercises methods/Program.cs
@@ -7,13 +7,9 @@ namespace exercises_methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"-10{MathUtilities.IsPrime(-10)}");
-            Console.WriteLine($"1{MathUtilities.IsPrime(1)}");
-            Console.WriteLine($"23{MathUtilities.IsPrime(23)}");
-            Console.WriteLine($"0{MathUtilities.IsPrime(0)}");
-            Console.WriteLine($"8{MathUtilities.IsPrime(8)}");
-            Console.WriteLine($"76{MathUtilities.IsPrime(76)}");
-            Console.WriteLine($"9{MathUtilities.IsPrime(9)}");
+            Console.WriteLine(MathUtilities.AreCoprime(8,9));
+            Console.WriteLine(MathUtilities.AreCoprime(8,10));
+            
         }
     }
 }
